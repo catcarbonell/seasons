f6f36f5c0b9d5dceeddd2cbeef676f832e57d20e
catcarbonell <catcarbonell@gmail.com>
2022-01-23 18:48:35 -0800

    Implemented states

--- src/index.js
+++ src/index.js
@@ -3,17 +3,31 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { 
+      lat: null, 
+      errorMessage: '' 
+    };
     
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude })
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message })
+      }
     );
   }
   render() {
-    return <div>Latitude: {this.state.lat} </div>
+    return (
+      <div>
+        {this.state.lat ? 
+          <h1>Latitude: {this.state.lat}</h1> 
+       : !this.state.lat && this.state.errorMessage ?
+        <h1>Error: {this.state.errorMessage}</h1>
+       : 
+       <h1>Loading...</h1> }
+      </div>
+    )
   }
 }
 
